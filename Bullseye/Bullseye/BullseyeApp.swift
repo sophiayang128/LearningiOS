de39b4e013322e11d0346a4fcb3798e1b9dfbf92
Jinyang Tang <jit098@ucsd.edu>
2021-08-25 23:49:44 -0400

    init commit for Bullseye

---
+++ Bullseye/Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Sophia Tang on 8/25/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye/Bullseye/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Sophia Tang on 8/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("🎯Put the Bullseye as close as you can to the target number!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
